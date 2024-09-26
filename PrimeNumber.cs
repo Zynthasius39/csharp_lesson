79cacf2df5cc6e7e3b045790f7c58a9d9c0a3ffa
Zynt <super.alekberov+gh@proton.me>
2024-09-27 00:37:08 +0400

    Added PrimeNumber, WeekDay

---
+++ PrimeNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class PrimeNumber
+    {
+        public static void Run()
+        {
+            START:
+            Console.WriteLine("Enter number range [from, to]: ");
+            UInt64 numFrom, numTo;
+            try
+            {
+                numFrom = Convert.ToUInt64(Console.ReadLine());
+                numTo = Convert.ToUInt64(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Enter valid number range! [0 - 18,446,744,073,709,551,615]");
+                goto START;
+            }
+            if (numFrom > numTo)
+            {
+                UInt64 Temp = numFrom;
+                numFrom = numTo;
+                numTo = Temp;
+            }
+            for (; numFrom <= numTo; numFrom++)
+                if (IsPrime(numFrom)) Console.Write(numFrom.ToString() + " ");
+        }
+
+        public static Boolean IsPrime(UInt64 num)
+        {
+            if (num == 0 || num == 1) return false;
+            for (UInt64 i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ WeekDay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class WeekDay
+    {
+        public static void Run()
+        {
+            Console.Write("Enter a week day [1-7]: ");
+            START:
+            String input = Console.ReadLine();
+
+            Int16 inputNum;
+            String[] weekdays = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            try
+            {
+                inputNum = Convert.ToInt16(input);
+            }
+            catch
+            {
+                Console.Write("Enter a valid week day [1-7]: ");
+                goto START;
+            }
+
+            if (inputNum > 7 || inputNum < 1)
+            {
+                Console.Write("Enter a day between [1-7]: ");
+                goto START;
+            }
+            Console.WriteLine(weekdays[inputNum - 1]);
+        }
+    }
+}
